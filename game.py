4fc7ca55fcef57394a8116de56e00fc9159f1345
Edijs Endijs Eglītis <110787421+EdijsEndijsEglitis@users.noreply.github.com>
2022-09-28 08:19:19 +0300

    Now game will repeat until player types ''Q"

--- game.py
+++ game.py
@@ -1,19 +1,37 @@
 import random
 
-x = ["rock", "paper", "scissors"]
 
-human_turn = input("Choose Rock/Paper/Scissors :")
-computer_turn = random.choice(x)
+turns = ["rock", "paper", "scissors"]
+player_wins = 0
+games_played = 0
 
-human_turn = human_turn.lower()
 
-if human_turn == "rock" and computer_turn == "scissors":
-    print("Human wins!")
-elif human_turn == "paper" and computer_turn == "rock":
-    print("Human wins!")
-elif human_turn == "scissors" and computer_turn == "paper":
-    print("Human wins!")
-elif human_turn == computer_turn:
-    print("It's a draw!")
-else:
-    print("Computer wins!")
+while(True):
+    human_turn = input("Choose Rock/Paper/Scissors or 'Quit' to quit :")
+    computer_turn = random.choice(turns)
+
+    human_turn = human_turn.lower()
+    print(f'Human: {human_turn} vs Computer: {computer_turn}')
+    if human_turn == "rock" and computer_turn == "scissors":
+        print("Human wins!")
+        player_wins += 1
+        games_played +=1
+    elif human_turn == "paper" and computer_turn == "rock":
+        print("Human wins!")
+        player_wins += 1
+        games_played +=1
+    elif human_turn == "scissors" and computer_turn == "paper":
+        print("Human wins!")
+        player_wins += 1
+        games_played +=1
+    elif human_turn == computer_turn:
+        print("It's a draw!")
+        games_played +=1
+    elif human_turn == "quit":
+        print(f'You won {player_wins}/{games_played} games')
+        print("Thanks for playing! Goodbye!")
+        break
+    else:
+        print("Computer wins!")
+        games_played +=1
+
