bddeb53dd645bfd38d19ebb73d883e706def7593
Edijs Endijs Eglītis <110787421+EdijsEndijsEglitis@users.noreply.github.com>
2022-09-28 09:54:44 +0300

    Now you cann see played games history

--- game.py
+++ game.py
@@ -1,37 +1,40 @@
 import random
-
-
 turns = ["rock", "paper", "scissors"]
-player_wins = 0
-games_played = 0
+player_turns = []
+computer_turns = []
 
 
 while(True):
     human_turn = input("Choose Rock/Paper/Scissors or 'Quit' to quit :")
     computer_turn = random.choice(turns)
 
+
+    if  human_turn == "quit":
+        print("Thanks for playing! Goodbye!")
+        break
+    
+    player_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+    
+
     human_turn = human_turn.lower()
     print(f'Human: {human_turn} vs Computer: {computer_turn}')
     if human_turn == "rock" and computer_turn == "scissors":
         print("Human wins!")
-        player_wins += 1
-        games_played +=1
     elif human_turn == "paper" and computer_turn == "rock":
         print("Human wins!")
-        player_wins += 1
-        games_played +=1
     elif human_turn == "scissors" and computer_turn == "paper":
         print("Human wins!")
-        player_wins += 1
-        games_played +=1
     elif human_turn == computer_turn:
         print("It's a draw!")
-        games_played +=1
-    elif human_turn == "quit":
-        print(f'You won {player_wins}/{games_played} games')
-        print("Thanks for playing! Goodbye!")
-        break
     else:
         print("Computer wins!")
-        games_played +=1
+    
+
+
+print(f'You have played {len(player_turns)} games' )
+print(player_turns)
+print(computer_turns)
+
+
 
